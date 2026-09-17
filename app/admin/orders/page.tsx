d625dbc50cd8a7f44012bb5a3393d856edb09ed0
Vipan <86885836+vipan2509@users.noreply.github.com>
2026-09-17 23:29:16 +0530

    deleting

--- app/admin/orders/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation'
-import { isAdminAuthenticated } from '@/lib/admin-auth'
-import OrdersDashboardClient from '@/components/orders-dashboard-client'
-
-export default async function OrdersDashboard() {
-  if (!(await isAdminAuthenticated())) redirect('/admin/login')
-  return <OrdersDashboardClient />
-}
